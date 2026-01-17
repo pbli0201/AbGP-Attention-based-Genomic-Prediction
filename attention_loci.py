243e44e373326ef81c4f0b2d87bcb8f7edcd3204
pbli0201 <136771236+pbli0201@users.noreply.github.com>
2026-01-17 15:12:29 +0800

    Update attention_loci.py

--- attention_loci.py
+++ attention_loci.py
@@ -4,6 +4,7 @@ from sklearn.kernel_ridge import KernelRidge
 from sklearn.model_selection import train_test_split
 from scipy.stats import pearsonr
 import pytorch_lightning as pl
+from scipy.special import softmax
 
 pl.seed_everything(9527)
 
@@ -13,19 +14,21 @@ t_cosine = pd.read_pickle('t_matrix')
 g_cosine.index = t_cosine.index
 g_matrix = pd.read_pickle('g_matrix')
 
-
 Q = np.dot(g_matrix, Genotype)
 K = np.dot(g_cosine, Genotype)
 K_T = K.T
 V = np.dot(t_cosine, Genotype)
 
 Attention_scores = np.dot(Q, K_T) / np.sqrt(592929)
-Attention_weigts = np.dot(Attention_scores, V)
-AW_colmean = np.mean(Attention_weigts, axis=0)
-AW = pd.DataFrame(AW_colmean, columns=['Values']).T
+Attention_weigts_raw = np.dot(Attention_scores, V)
 
-Attention_scores=pd.DataFrame(Attention_scores)
-Attention_weigts=pd.DataFrame(Attention_weigts)
+AW_colmean_raw = np.mean(Attention_weigts_raw, axis=0)
+AW_final = softmax(AW_colmean_raw)
+
+AW = pd.DataFrame(AW_final, columns=['Values']).T
+
+Attention_scores = pd.DataFrame(Attention_scores)
+Attention_weigts = pd.DataFrame(Attention_weigts_raw)
 
 Attention_scores.to_pickle('Attention_scores.pkl')
 Attention_weigts.to_pickle('Attention_weigts.pkl')
@@ -41,7 +44,6 @@ for threshold in thresholds:
     indices = np.where(values >= current_threshold)[0]
 
     filtered_genotype = Genotype.iloc[:, indices]
-    # Phenotype = pd.read_pickle('adjusted_phe')
     Phenotype = pd.read_csv('iid_218.csv')
     Phe1 = Phenotype.iloc[:, 1]
 
